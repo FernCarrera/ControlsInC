4f31533a1a7bf4c6fcc232ba1ac8287b115cf8db
FernCarrera <carrerafct@gmail.com>
2020-02-26 16:51:49 -0800

    assert norm_angle

--- lib/control.c
+++ lib/control.c
@@ -2,24 +2,9 @@
 #include <math.h>
 #include "control.h"
 #include <assert.h>
-
+#include <stdio.h>
 // init vehicle state
 State vehicle_state = {0.0,0.0,0.0,0.0};
-
-
-/*
-	Updates the state of the vehicle, utilizing 
-	a bicycle model
-
-	@INPUTS: State struct, Accel [m/s^2], delta: steering angle [deg]
-
-*/
-
-void update(State *state,float accel,float delta)
-{
-
-
-}
 
 /*
 	Clips value based on max and min
@@ -33,10 +18,44 @@ static float clip(float n, float min, float max)
 
 }
 
+
+/*
+	keeps angle within -2pi and 2pi
+*/
+static float normalize_angle(float angle)
+{
+	while (angle > PI){
+		angle -= 2.0 * PI;
+	}
+	while (angle < -PI){
+		angle += 2.0 * PI;
+	}
+	return angle;
+}
+
+/*
+	Updates the state of the vehicle, utilizing 
+	a bicycle model
+
+	@INPUTS: State struct, Accel [m/s^2], delta: steering angle [deg]
+
+*/
+void update(State* state,float accel,float delta)
+{
+	delta = clip(delta,-MAX_STEERING_ANGLE,MAX_STEERING_ANGLE);
+	
+	state->x += state->v * cos(state->head)*DT;
+	state->y += state->v * sin(state->head)*DT;
+	state->head += state->v / WHEEL_BASE * tan(delta) * DT;
+	state->head = normalize_angle(state->head);
+}
+
+
 int main()
 {
 	float og = 3.5;
 	assert(clip(og,5,10) == 5);
-	
+	float value = 3.141593;
+	assert(normalize_angle(3.0*PI) == value);
 
 }
--- lib/control.h
+++ lib/control.h
@@ -2,6 +2,7 @@
 #define CONTROL_H
 
 #include <stdlib.h>
+#include <math.h>
 /*
 	Struct defining the vehicle's state
 */
@@ -13,6 +14,10 @@ typedef struct State{
 	float v;
 }State;
 
+const float MAX_STEERING_ANGLE =  40*(M_PI/180);
+const float DT =  0.1;
+const float WHEEL_BASE = 1.9;
+const float PI = 3.141593;
 
 /*
 	Updates the state of the vehicle, utilizing 
@@ -24,12 +29,11 @@ typedef struct State{
 void update(State *state,float accel,float delta);
 
 
-/*
-	Clips value based on max and min
 	
-	@INPUTS: n: value, min: min value, max: max value
 
-*/
-static float clip(float n, float min, float max);
+
+
+
+
 
 #endif
